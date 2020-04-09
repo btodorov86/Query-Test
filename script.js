e323b92135906c621c691ad617a0fe4d0785276d
Borislav Todorov <60582465+btodorov86@users.noreply.github.com>
2020-04-09 19:24:14 +0300

    add handlebars

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+
+
 let btn = $('#btn');
-
-
-
 
 $('#btn').click(function (e) {
     e.preventDefault();
+    e.stopPropagation();
     let town = document.getElementById('input').value;
     let URL = `https://api.worldweatheronline.com/premium/v1/weather.ashx?key=d32da97ae5564c43a3f72157203103&q=${town}&format=json&num_of_days=1`;
     document.getElementById('input').value = '';
@@ -24,7 +24,10 @@ $('#btn').click(function (e) {
 
             let weatherObj = {
 
-                date: weatherInfo.date,
+                date: (function () {
+                    let [year, mount, day] = weatherInfo.date.split('-')
+                    return `${day}-${mount}-${year}`
+                })(),
                 tempMin: weatherInfo.mintempC,
                 tempMax: weatherInfo.maxtempC,
                 avgTemp: weatherInfo.avgtempC,
@@ -32,9 +35,33 @@ $('#btn').click(function (e) {
                 sunset: weatherInfo.astronomy[0].sunset
 
             };
+            
+            // Handlebars add element
+            
+        //     let handlebarsObj = {
+        //         sumbol: '&#9728',
+        //         town: weatherObj.date,
+        //         gradusi: weatherObj.avgTemp,
+        //         sunrise: weatherObj.sunrise
+
+        //     }
+
+        //     let templateStr = '<div class="forecast">\
+        //     <span class="condition symbol">{{symbol}}</span>\
+        //     <span class="condition">\
+        //         <span class="forecast-data">{{town}}</span>\
+        //         <span class="forecast-data">{{gradusi}}</span>\
+        //         <span class="forecast-data">{{sunrise}}</span>\
+        //     </span>\
+        // </div>';
+
+        // let template = Handlebars.compile(templateStr);
+        // let redyTemp = template(handlebarsObj)
+        // document.querySelector('#current').innerHTML = redyTemp;
+            
             // document.querySelector('h2').textContent = `Forecast for ${weatherObj.date}, ${cityInfo}`;
-            $('h2').text(`Forecast for ${weatherObj.date}, ${cityInfo}`);
-            $('h2').css('display', 'block');
+            $('#current .label').text(`Forecast for ${weatherObj.date}, ${cityInfo}`);
+            // $('h2').css('display', 'block');
             console.log(weatherObj);
             (function sendDataToFirebase() {
                 let postOptions = {
@@ -71,8 +98,6 @@ $('#btn2').click(function (e) {
             solve(data);
 
         }
-
-
     });
     function solve(data) {
         console.log(Object.entries(data));
@@ -96,6 +121,7 @@ $('#btn3').click(function (e) {
 
         }
     })();
-})
+});
+
 
 
